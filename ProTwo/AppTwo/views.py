351661209550313b2d54a8ca2c76bf6d1eab3735
AbYahaya <yabdulrauf911@gmail.com>
2024-07-04 21:32:37 +0100

    Still on practice

---
+++ ProTwo/AppTwo/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here
+def index(request):
+    return HttpResponse("<em>My Second App</em>")
+
+def help(request):
+    help_dict = {'help_me': 'This is just me testing!'}
+    return render(request, 'AppTwo/help.html', context=help_dict)
